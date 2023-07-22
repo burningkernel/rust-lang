5681afcb381e9b391ae06e1db5a29122d2f5dacf
Burningkernel <burningkernel@gmail.com>
2023-07-22 12:33:53 +0800

    Add buf_builder

---
+++ single/generic_read.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{BufReader, Read, Result};
+
+struct MyReader<R> {
+    reader: R,
+    buf: String,
+}
+
+impl<R> MyReader<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf: String::with_capacity(1024),
+        }
+    }
+}
+
+impl<R> MyReader<R>
+where
+    R: Read,
+{
+    fn process(&mut self) -> Result<usize> {
+        self.reader.read_to_string(&mut self.buf)
+    }
+}
+
+
+fn main() {
+    let f = File::open("/etc/hosts").unwrap();
+    let mut reader = MyReader::new(BufReader::new(f));
+    let size = reader.process().unwrap();
+    println!("total size read: {size}");
+    println!("Read string:\n{}", &reader.buf);
+}
